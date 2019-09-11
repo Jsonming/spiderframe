cadca67bcdf65cb8cc6c1ef2df8b809e35ff18f3
jsonming <ymmbjcz@126.com>
2019-09-11 15:58:53 +0800

    '20190911'

--- spiderframe/spiders/india_hindi_link.py
+++ spiderframe/spiders/india_hindi_link.py
@@ -1,31 +1,126 @@
 # -*- coding: utf-8 -*-
 import scrapy
-import json
-from spiderframe.items import ImgsItem
 
+from spiderframe.items import ImgsItem
 
 
 class IndiaHindiLinkSpider(scrapy.Spider):
     name = 'india_hindi_image'
     allowed_domains = ['hindi.webdunia.com']
-
-    # start_urls = [
-    #     'https://hindi.webdunia.com/indian-festivals?utm_source=Top_Nav_Article&utm_medium=Site_Internal/1/{}'.format(i)
-    #     for i in range(2, 4)]
-
-    def start_requests(self):
-        for i in range(2, 100):
-            url = "https://hindi.webdunia.com/core/home-page/more-category-news"
-            form_data = {"page": str(i), "item_per_page": '', "category_id": '1060300000', "category_creation_id": '10603'}
-
-            yield scrapy.FormRequest(url=url, formdata=form_data, callback=self.parse, dont_filter=True)
+    start_urls = [
+        'https://hindi.webdunia.com/ganesh-utsav-special?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.com/indian-festivals?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.com/indian-religion?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.com/shirdi-sai-baba?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.com/astrology-hindi#498?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.com/mahabharat?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.com/sanatan-dharma?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.com/religious-journey#999?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'http://hindi.webdunia.com/religion/religion/hindu/ramcharitmanas/?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.com/religious-unbelievable-facts?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.com/religious-article?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.com/bollywood-hindi-news?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.com/bollywood-gossip?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.com/bollywood-movie-review?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.com/bollywood-article?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.com/bollywood-movie-preview?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.com/bollywood-focus?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.com/dilip-kumar-special?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.com/bollywood-khul-jaa-sim-sim?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.com/salman-khan-special?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.com/sunny-leone-special?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.com/shahrukh-khan-special?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.com/indian-television?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.com/bollywood-celebrity-interview#272?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.com/entertainment?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.com/astrology-hindi?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.com/birthday-astrology?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.com/astrology-muhurat?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'http://astrology.webdunia.com/hindi/predictions/Predictions.aspx?rashiId=1&rashiMode=1?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.com/astrology-2019?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.com/astrology-zodiac-signs?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.com/lal-kitab?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.com/astrology-tantra-mantra-yantra?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.com/vastu-fengshui?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'http://astrology.webdunia.com/hindi/tarot/?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'http://astrology.webdunia.com/hindi/Matchmaking/?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'http://astrology.webdunia.com/hindi/Horoscope/?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'http://hindi.webdunia.com/astrology/ramshalaka/?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'http://hindi.webdunia.com/astrology/choghadia/index.htm?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.com/astrology-articles?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.com/learn-astrology?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.com/astrology-nakshatra-sign?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.com/astrology-navagraha?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.com/celibrity-horoscope?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.com/astrology-gems-stone?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.com/cricket-news?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.com/sports-update?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.com/sports-coverage?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.com/cricket-score-card?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'http://hindi.webdunia.com/icc-rankings?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.com/cricket-fixtures?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'http://utilities.webdunia.com/downloadzone.html#wd_ticker?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.com/hindi-news?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.com/latest-hindi-news?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.com/national-hindi-news?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.com/international-hindi-news?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.com/regional-hindi-news?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.com/madhya-pradesh?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.com/uttar-pradesh?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.com/crime?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.com/web-viral?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.com/latest-cricket-news#568?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.com/business?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.com/mobile-news?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.com/lifestyle?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.com/health-tips?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.com/yoga?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.com/nri#957?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.com/recipe?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.com/women?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.com/kids-world?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.com/literature?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.com/romance?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://www.youtube.com/user/webduniaindia?utm_source=Top_Nav_Listing&utm_medium=Site_Internal',
+        # 'https://hindi.webdunia.comhindi.webdunia.com/channels/list?utm_source=Top_Nav_Listing&utm_medium=Site_Internal'
+    ]
 
     def parse(self, response):
         images = response.xpath(r'//div[@id="contentListingData"]/div//img/@src').extract()
-        images = response.xpath(r'//img/@src').extract()
-        image_url = ["https:" + url for url in images]
+        if images:
+            category_id = response.xpath('//input[@id="category_id"]/@value').extract()[0]
+            category_creation_id = response.xpath('//input[@id="category_creation_id"]/@value').extract()[0]
+            page = '1'
+        else:
+            images = response.xpath(r'//img/@src').extract()
+            category_id = response.meta.get("category_id")
+            category_creation_id = response.meta.get("category_creation_id")
+            page = response.meta.get("page")
+            page = str(int(page) + 1)
 
-        item = ImgsItem()
-        item["category"] = "hindi"
-        item["image_urls"] = image_url
-        yield item
+        if images:
+            image_url = ["https:" + url for url in images]
+            item = ImgsItem()
+            item["category"] = category_creation_id
+            item["image_urls"] = image_url
+            yield item
+
+            url = "https://hindi.webdunia.com/core/home-page/more-category-news"
+            form_data = {"page": page, "item_per_page": '', "category_id": category_id,
+                         "category_creation_id": category_creation_id}
+            yield scrapy.FormRequest(url=url, formdata=form_data, meta={"page": page, "category_id": category_id,
+                                                                        "category_creation_id": category_creation_id},
+                                     callback=self.parse, dont_filter=True)
+
+    # def parse_item(self, response):
+    #     image_url = ["https:" + url for url in images]
+    #     item = ImgsItem()
+    #     item["category"] = "hindi"
+    #     item["image_urls"] = image_url
+    #     yield item
+
+        # url = "https://hindi.webdunia.com/core/home-page/more-category-news"
+        # form_data = {"page": "1", "item_per_page": '', "category_id": category_id,
+        #              "category_creation_id": category_creation_id}
+        # yield scrapy.FormRequest(url=url, formdata=form_data, meta={"page": 1}, callback=self.parse_item,
+        #                          dont_filter=True)
--- spiderframe/spiders/video_bilibili_link.py
+++ spiderframe/spiders/video_bilibili_link.py
@@ -1,26 +1,34 @@
 # -*- coding: utf-8 -*-
-import scrapy
 import re
+import json
+import scrapy
 from spiderframe.items import SpiderframeItem
 
 
 class VideoBilibiliLinkSpider(scrapy.Spider):
     name = 'video_bilibili_link'
     allowed_domains = ['search.bilibili.com']
-    start_urls = ['https://search.bilibili.com/all?keyword=化妆视频&page=1']
+
+    def __init__(self, keyword="抢劫监控", *args, **kwargs):
+        super(VideoBilibiliLinkSpider, self).__init__(*args, **kwargs)
+        self.keyword = keyword
+
+    def start_requests(self):
+        url = 'https://search.bilibili.com/all?keyword={}'.format(self.keyword)
+        yield scrapy.Request(url=url, callback=self.parse, dont_filter=True, meta={'page': 1})
 
     def parse(self, response):
-        links = response.xpath('//*[@id="server-search-app"]/div[2]/div[2]/div/div[2]/ul/li/div/div[1]/a/@href').extract()
+
+        links = response.xpath('//li[@class="video-item matrix"]/a/@href').extract()
         for link in links:
             if link.startswith('//'):
                 link = "http:" + link
             item = SpiderframeItem()
             item['url'] = link
-
             yield item
 
-        current_page_url = response.url.split("=")
-        current_page = current_page_url[-1]
-        if int(current_page) <= 50:
-            next_page_url = "https://search.bilibili.com/all?keyword=化妆视频&page={}".format(int(current_page) + 1)
-            yield scrapy.Request(url=next_page_url, callback=self.parse, dont_filter=True)
+        current_page = response.meta.get("page")
+        if current_page <= 50 and links:
+            next_page_num = int(current_page) + 1
+            next_page_url = "https://search.bilibili.com/all?keyword={}&page={}".format(self.keyword, next_page_num)
+            yield scrapy.Request(url=next_page_url, callback=self.parse, dont_filter=True, meta={'page': next_page_num})
