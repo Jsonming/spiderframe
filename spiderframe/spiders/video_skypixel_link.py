6ca753b843e4c969f0ad8878658f98d5fe47dfd4
XinYuCode <1582166721@qq.com>
2019-09-18 14:46:56 +0800

    20190918d

--- spiderframe/spiders/video_skypixel_link.py
+++ spiderframe/spiders/video_skypixel_link.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import demjson
 import scrapy
+from urllib.parse import quote
 
 from spiderframe.items import SpiderframeItem
 
@@ -8,22 +9,34 @@ from spiderframe.items import SpiderframeItem
 class ImageSpider(scrapy.Spider):
     name = 'video_skypixel_link'
 
+    def __init__(self, category="门", *args, **kwargs):
+        super(ImageSpider, self).__init__(*args, **kwargs)
+        self.category = category
+
     def start_requests(self):
-        for i in range(0, 40, 20):
-            url = "https://www.skypixel.com/api/v2/works?lang=zh-Hans&platform=web&device=desktop&sort=hot&" \
-                  "filter=featured:true&limit=20&offset={}".format(i)
+        for i in range(0, 20, 20):
+            url="https://www.skypixel.com/api/v2/searches/videos?lang=zh-Hans&platform=web&device=desktop&keyword={category}&limit=20&offset={i}".format(category=quote(self.category), i=i)
             yield scrapy.Request(url=url, callback=self.parse, dont_filter=True)
+
 
     def parse(self, response):
         page = response.text
         json_content = demjson.decode(page)
         json_contents = json_content["data"]
         for json_list in json_contents["items"]:
-            cdn_url = json_list.get("cdn_url")
-            if type(cdn_url) == dict:
-                large_mp4 = cdn_url.get("large")
-                item = SpiderframeItem()
-                item["url"] = large_mp4
-                yield item
-            else:
-                print("error: ")
+            slug = json_list.get("slug")
+            url="https://www.skypixel.com/api/v2/videos/"+slug+"?lang=zh-Hans&platform=web&device=desktop"
+            yield scrapy.Request(url=url, callback=self.parse_url, dont_filter=True)
+
+    def parse_url(self, response):
+        page = response.text
+        json_content = demjson.decode(page)
+        json_contents = json_content["data"]
+        json_list = json_contents["item"]
+        cdn_url = json_list.get("cdn_url")
+        large_mp4=cdn_url.get("large")
+        item = SpiderframeItem()
+        item["url"] = large_mp4
+        yield item
+
+
