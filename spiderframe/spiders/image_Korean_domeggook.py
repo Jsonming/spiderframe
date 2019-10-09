4a3920df4bc5956ef01307c1f13039fdf02e65f0
XinYuCode <1582166721@qq.com>
2019-10-09 17:57:47 +0800

    20191009b

---
+++ spiderframe/spiders/image_Korean_domeggook.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import re
+from spiderframe.items import ImgsItem
+
+
+class ImageSpider(scrapy.Spider):
+    name = 'image_domeggook'
+    start_urls = ["http://domeggook.com/main/"]
+
+    def __init__(self, *args, **kwargs):
+        super(ImageSpider, self).__init__(*args, **kwargs)
+
+    def parse(self, response):
+        img_urls = response.xpath('//li[@class="mainCate"]/a/@href').extract()
+        for img_url in img_urls:
+            img_url="http://domeggook.com"+img_url
+            yield scrapy.Request(url=img_url, callback=self.parse_url, dont_filter=True)
+
+    def parse_url(self, response):
+        img_urls = response.xpath('//ul[@class="subcate"]//a/@href').extract()
+        for img_url in img_urls:
+            img_url="http://domeggook.com"+img_url
+            yield scrapy.Request(url=img_url, callback=self.parse_img, dont_filter=True)
+
+    def parse_img(self, response):
+        category = response.xpath('//h2//text()').extract()
+        next_urls = response.xpath('//a[@class="bgYUI next"]/a/@href').extract()
+        for next_url in next_urls:
+            yield scrapy.Request(next_url, callback=self.parse_img)
+        img_urls = response.xpath('//ol[@class="lItemList"]/li/a/@href').extract()
+        for img_url in img_urls:
+            img_url="http://domeggook.com"+img_url
+            yield scrapy.Request(url=img_url, callback=self.parse_content, dont_filter=True,meta={'category': category})
+
+
+    def parse_content(self, response):
+        category = response.meta['category']
+        img_urls = response.xpath('//img[@class="mainThumb"]/@src').extract()
+        item = ImgsItem()
+        item["image_urls"] = img_urls
+        item["category"] = category[0]
+        yield item
