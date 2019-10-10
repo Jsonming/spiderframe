1676b725d48724adff27c98767eedb907d611f56
XinYuCode <1582166721@qq.com>
2019-10-10 16:34:35 +0800

    20191010a

--- spiderframe/spiders/image_Korean_yes24.py
+++ spiderframe/spiders/image_Korean_yes24.py
@@ -12,34 +12,6 @@ class ImageSpider(scrapy.Spider):
 
     def start_requests(self):
         url = "http://www.yes24.com/"
-       #  headers = {
-       #      "User-Agent": "Mozilla/5.0 (Windows NT 6.1; W…) Gecko/20100101 Firefox/69.0",
-       #      "Cookie":"PCID=15705230219801830925000; cartCookieCnt=0; RecentViewGoods=; RecentViewInfo=NotCookie%3DY%26Interval%3D5; wcs_bt=s_1b6883469aa6:1570582801; __utma=12748607.1824039958.1570523097.1570523097.1570582802.2; __utmz=12748607.1570523097.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); _ga=GA1.2.1824039958.1570523097; _gid=GA1.2.2003353712.1570523707; yes24_glbola_redirect=value=https%3A%2F%2Fcn.yes24.com%2F; HTTP_REFERER=; WiseLogParam=Null; __utmc=12748607; ASP.NET_SessionId=v4i2a345en2kgjr3oo25pkur; __utmb=12748607.1.10.1570582802; __utmt=1",
-       #      # "Accept":"text / html, application / xhtml + xm…plication / xml;q = 0.9, * / *;q = 0.8",
-       #      # "Accept - Encoding":"gzip, deflate",
-       #      # "Accept - Language":"zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
-       #      # "Cache - Control":"max - age = 0",
-       #      "Connection":"keep-alive",
-       #      "Host":"www.yes24.com"	,
-       #      "Upgrade-Insecure-Requests":"1"
-       # }
-        # cookie={
-        #         "__utma":"12748607.1824039958.1570523097.1570523097.1570523097.1",
-        #         "__utmb":"12748607.5.10.1570523097",
-        #         "__utmc":"12748607",
-        #         "__utmz":"12748607.1570523097.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none)",
-        #         "_gz":"GA1.2.1824039958.1570523097",
-        #         "_gid": "GA1.2.2003353712.1570523707",
-        #         "ASP.NET_SessionId":"v4i2a345en2kgjr3oo25pkur",
-        #         "cartCookieCnt":"0",
-        #         "HTTP_REFERER"
-        #         "PCID":"15705230219801830925000",
-        #         "RecentViewGoods"
-        #         "RecentViewInfo":"NotCookie=Y&Interval=5",
-        #          "wcs_bt":"s_1b6883469aa6:1570526170",
-        #          "WiseLogParam":"Null",
-        #          "yes24_glbola_redirect":"value=https://cn.yes24.com/"
-        # }
         headers ={
          "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36",
          "Cookie": "PCID=15705230219801830925000; cartCookieCnt=0; RecentViewGoods=; RecentViewInfo=NotCookie%3DY%26Interval%3D5; wcs_bt=s_1b6883469aa6:1570591442; __utma=12748607.1824039958.1570523097.1570585177.1570591444.4; __utmz=12748607.1570523097.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); _ga=GA1.2.1824039958.1570523097; _gid=GA1.2.2003353712.1570523707; yes24_glbola_redirect=value=https%3A%2F%2Fcn.yes24.com%2F; HTTP_REFERER=; WiseLogParam=Null; __utmc=12748607; ASP.NET_SessionId=v4i2a345en2kgjr3oo25pkur; __utmb=12748607.1.10.1570591444; __utmt=1"
@@ -59,14 +31,12 @@ class ImageSpider(scrapy.Spider):
             yield scrapy.Request(url=img_url, callback=self.parse_img, dont_filter=True)
 
     def parse_img(self, response):
-        category_url = response.url
-        urls = re.split("/", category_url)
-        category = urls[-1]
+        category = response.xpath('//em[@class="cate_tit"]//text()').extract()
         next_urls = response.xpath('//a[@class="bgYUI next"]/a/@href').extract()
         for next_url in next_urls:
+            next_url="http://www.yes24.com"+next_url
             yield scrapy.Request(next_url, callback=self.parse_img)
         img_urls = response.xpath('//span[@class="imgBdr"]/a/@href').extract()
-        # category = response.xpath('//h3[@class="cateTit_txt"]//text()').extract()
         for img_url in img_urls:
             img_url="http://www.yes24.com/"+img_url
             yield scrapy.Request(url=img_url, callback=self.parse_content, dont_filter=True,meta={'category': category})
@@ -79,5 +49,10 @@ class ImageSpider(scrapy.Spider):
         item = ImgsItem()
         # if "http://image.yes" in img_urls:
         item["image_urls"] = img_urls
-        item["category"] = category
+        category = category[0]
+        if "/" in category:
+            category = re.sub("/", "", category)
+            item["category"] = category.replace(" ","")
+        else:
+            item["category"] = category.replace(" ","")
         yield item
