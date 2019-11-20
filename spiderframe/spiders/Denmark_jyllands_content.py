509331a3b0440c85f7c77da17e54920e90b18960
XinYuCode <1582166721@qq.com>
2019-11-20 17:42:59 +0800

    20191120e

---
+++ spiderframe/spiders/Denmark_jyllands_content.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from spiderframe.items import SpiderframeItem
+from scrapy_redis.spiders import RedisSpider
+
+
+class DenmarkJyllandsContentSpider(RedisSpider):
+    name = 'Denmark_jyllands_content'
+    allowed_domains = ['jyllands-posten.dk']
+    start_urls = ['https://jyllands-posten.dk/debat/blogs/camillaburgwald/ECE11698930/hjernen-har-brug-for-ro-og-hvile-men-teenagere-er-online-op-til-seks-timer-om-dagen-hvem-skal-laere-dem-at-slukke/']
+
+    redis_key = 'Denmark_jyllands_link'
+
+    custom_settings = {
+        'REDIS_HOST': '123.56.11.156',
+        'REDIS_PORT': 6379,
+        'REDIS_PARAMS': {
+            'password': '',
+            'db': 0
+        },
+    }
+
+    def parse(self, response):
+        title = response.xpath('//h1/text()').extract()
+        content = response.xpath('//p/text()').extract()
+        content = ''.join(content)
+        content = content.replace(" ","")
+        item = SpiderframeItem()
+        item['url'] = response.url
+        item['category'] = response.url.split('/')[5]
+        item['title'] = ''.join(title)
+        item['content'] = content
+        # print(item)
+        yield item
+
---
+++ spiderframe/spiders/Denmark_jyllands_link.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import re
+from spiderframe.items import SpiderframeItem
+
+
+class DenmarkJyllandsLinkSpider(scrapy.Spider):
+    name = 'Denmark_jyllands_link'
+    allowed_domains = ['jyllands-posten.dk']
+    start_urls = ["https://jyllands-posten.dk"]
+
+    def parse(self, response):
+        patterns = response.xpath('//div[@class="siteMenuNavItem"]//a[@class="siteMenuNavItemLink"]/@href').extract()
+        for link in patterns:
+            # pattern=pattern.split('/')[3:]
+            # print(pattern)
+            # # link = "https://jyllands-posten.dk{pattern}/?widget=article&widgetId=8116054&subview=ajaxList&templateName=gridCol620&showBreaker=false&shown=12&pageSize=20".format(pattern=pattern)
+            # # print(link)
+            yield scrapy.Request(url=link, callback=self.parse_url, dont_filter=True)
+
+    def parse_url(self, response):
+        links = response.xpath('//h2/a/@href').extract()
+        for link in links:
+            item = SpiderframeItem()
+            item['url'] = link
+            print(item)
+            # yield item
+
+
