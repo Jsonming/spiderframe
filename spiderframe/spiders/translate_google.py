4c543eb97f76a5442f9c4015d45ecafe424b6679
Jsonming <ymmbjcz@126.com>
2020-02-18 15:39:21 +0800

    20200218a

--- spiderframe/spiders/translate_google.py
+++ spiderframe/spiders/translate_google.py
@@ -15,7 +15,7 @@ class TranslateGoogleSpider(scrapy.Spider):
     start_urls = ['http://translate.google.cn/']
 
     def start_requests(self):
-        with open(r'D:\datatang\spiderframe\spiderframe\files\简单句单词总.txt', 'r', encoding='utf8')as f:
+        with open(r'F:\Yang\spiderframe\spiderframe\files\text.txt', 'r', encoding='utf8')as f:
             for key_word in f:
                 keyword = key_word.strip()
                 pj = Py4Js()
@@ -26,17 +26,23 @@ class TranslateGoogleSpider(scrapy.Spider):
                 yield scrapy.Request(url=url, callback=self.parse, meta={'keyword': keyword}, dont_filter=True)
 
     def parse(self, response):
-        dr = re.compile(r'<[^>]+>', re.S)
-        resp = json.loads(response.text)[-1][0]
-        for sentence in resp:
-            if sentence:
-                sentence = sentence[0]
-                if isinstance(sentence, str):
-                    sent = dr.sub('', sentence).strip()
-                    md = md5(sent)
-                    item = SpiderframeItem()
-                    item['content'] = sent
-                    item['title'] = response.meta.get("keyword")
-                    item['category'] = 'google'
-                    item['item_id'] = md
-                    yield item
+        # 抓取读音
+        resp = json.loads(response.text)[0][-1]
+        pronunciation = resp[-1]
+        print(pronunciation)
+
+        # 抓取例句
+        # dr = re.compile(r'<[^>]+>', re.S)
+        # resp = json.loads(response.text)[-1][0]
+        # for sentence in resp:
+        #     if sentence:
+        #         sentence = sentence[0]
+        #         if isinstance(sentence, str):
+        #             sent = dr.sub('', sentence).strip()
+        #             md = md5(sent)
+        #             item = SpiderframeItem()
+        #             item['content'] = sent
+        #             item['title'] = response.meta.get("keyword")
+        #             item['category'] = 'google'
+        #             item['item_id'] = md
+        #             yield item
