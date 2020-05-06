b5a02e5f9a39f843ce8a8671ef01b768ec3a3d2e
jsonming <ymmbjcz@126.com>
2020-05-06 13:54:43 +0800

    谷歌tts读音抓取

--- spiderframe/spiders/translate_google.py
+++ spiderframe/spiders/translate_google.py
@@ -17,57 +17,64 @@ class TranslateGoogleSpider(scrapy.Spider):
     }
 
     def start_requests(self):
-        # with open(r'D:\Workspace\workspace\work\English_word\lower_word.txt', 'r', encoding='utf8')as f:
-        #     for key_word in f.readlines()[:1]:
-        #         keyword = key_word.strip()
-        #         keyword = 'good'
-        #         pj = Py4Js()
-        #         tk = pj.get_tk(keyword)
-        #         url = "https://translate.google.cn/translate_a/single?client=webapp&sl=en&tl=zh-CN&hl=zh-CN&dt=at&" \
-        #               "dt=bd&dt=ex&dt=ld&dt=md&dt=qca&dt=rw&dt=rm&dt=ss&dt=t&ssel=3&tsel=3&kc=0" \
-        #               "&tk={tk}&q={keyword}".format(**locals())
-        #         yield scrapy.Request(url=url, callback=self.parse, dont_filter=True)
+        with open(r'C:\Users\Administrator\Desktop\tem.txt', 'r', encoding='utf8')as f:
+            for key_word in f.readlines():
+                keyword = key_word.strip()
+                keyword_len = len(keyword)
+                pj = Py4Js()
+                tk = pj.get_tk(keyword)
+                # url = "https://translate.google.cn/translate_a/single?client=webapp&sl=en&tl=zh-CN&hl=zh-CN&dt=at&" \
+                #       "dt=bd&dt=ex&dt=ld&dt=md&dt=qca&dt=rw&dt=rm&dt=ss&dt=t&ssel=3&tsel=3&kc=0" \
+                #       "&tk={tk}&q={keyword}".format(**locals())
+                url = 'https://translate.google.cn/translate_tts?ie=UTF-8&q={keyword}&tl=en&total=1&idx=0' \
+                      '&textlen={keyword_len}&tk={tk}&client=webapp&prev=input'.format(**locals())
+                yield scrapy.Request(url=url, callback=self.parse, dont_filter=True, meta={"keyword": keyword})
 
-        ssdb_con = SSDBCon().connection()
-        for i in range(70000):
-            item = ssdb_con.lpop("google_word_urls")
-            keyword = item.decode("utf8")
-            pj = Py4Js()
-            tk = pj.get_tk(keyword)
-            url = "https://translate.google.cn/translate_a/single?client=webapp&sl=en&tl=zh-CN&hl=zh-CN&dt=at&" \
-                  "dt=bd&dt=ex&dt=ld&dt=md&dt=qca&dt=rw&dt=rm&dt=ss&dt=t&ssel=3&tsel=3&kc=0" \
-                  "&tk={tk}&q={keyword}".format(**locals())
-
-            print(url)
-            yield scrapy.Request(url=url, callback=self.parse, dont_filter=True)
-        ssdb_con.close()
+        # ssdb_con = SSDBCon().connection()
+        # for i in range(70000):
+        #     item = ssdb_con.lpop("google_word_urls")
+        #     keyword = item.decode("utf8")
+        #     pj = Py4Js()
+        #     tk = pj.get_tk(keyword)
+        #     url = "https://translate.google.cn/translate_a/single?client=webapp&sl=en&tl=zh-CN&hl=zh-CN&dt=at&" \
+        #           "dt=bd&dt=ex&dt=ld&dt=md&dt=qca&dt=rw&dt=rm&dt=ss&dt=t&ssel=3&tsel=3&kc=0" \
+        #           "&tk={tk}&q={keyword}".format(**locals())
+        #
+        #     print(url)
+        #     yield scrapy.Request(url=url, callback=self.parse, dont_filter=True)
+        # ssdb_con.close()
 
     def parse(self, response):
-        # 抓取读音
-        word = response.url.split("=")[-1]
-        resp = json.loads(response.text)
-        word_tag = resp[0][0][1]
-        resp = resp[0][-1]
-        en_phonetic, am_phonetic = '', ''
-        if len(resp) == 4:
-            pronun = resp[-1].split(",")
-            if pronun:
-                pronunciation = ["[" + item + "]" for item in pronun]
-                if len(pronunciation) == 2:
-                    en_phonetic = pronunciation[0]
-                    am_phonetic = pronunciation[1]
-                elif len(pronunciation) == 1:
-                    en_phonetic = pronunciation[0]
-                else:
-                    print("*" * 200)
-                    print(pronunciation)
+        # 抓读音
+        with open('{}.mp3'.format(response.meta.get("keyword")), 'wb')as f:
+            f.write(response.body)
 
-        item = SpiderframeItem()
-        item['title'] = word  # title  字段 存单词
-        item['category'] = word_tag  # category 存显示的单词  google没有跳转现实
-        item['content'] = en_phonetic  # content 字段存 英式英语
-        item['item_name'] = am_phonetic  # category 字段  美式英语
-        yield item
+
+        # 抓取读音
+        # word = response.url.split("=")[-1]
+        # resp = json.loads(response.text)
+        # word_tag = resp[0][0][1]
+        # resp = resp[0][-1]
+        # en_phonetic, am_phonetic = '', ''
+        # if len(resp) == 4:
+        #     pronun = resp[-1].split(",")
+        #     if pronun:
+        #         pronunciation = ["[" + item + "]" for item in pronun]
+        #         if len(pronunciation) == 2:
+        #             en_phonetic = pronunciation[0]
+        #             am_phonetic = pronunciation[1]
+        #         elif len(pronunciation) == 1:
+        #             en_phonetic = pronunciation[0]
+        #         else:
+        #             print("*" * 200)
+        #             print(pronunciation)
+        #
+        # item = SpiderframeItem()
+        # item['title'] = word  # title  字段 存单词
+        # item['category'] = word_tag  # category 存显示的单词  google没有跳转现实
+        # item['content'] = en_phonetic  # content 字段存 英式英语
+        # item['item_name'] = am_phonetic  # category 字段  美式英语
+        # yield item
         # print(item)
 
         # 抓取例句
